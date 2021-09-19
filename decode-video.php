a7ae83ef1afe5b2c363aa776a7b248aff94b628b
Gil Megidish <gil@megidish.net>
2021-09-20 00:09:18 +0300

    Added some comments

--- decode-video.php
+++ decode-video.php
@@ -2,10 +2,23 @@
 
 	$im = imagecreatetruecolor(640, 480);
 
-	//$fp = fopen("16.EIDOS", "rb"); fseek($fp, 0x23);
+	$fp = fopen("16.EIDOS", "rb"); fseek($fp, 0x23);
 	//$fp = fopen("16.INTRO", "rb"); fseek($fp, 0x74061);
-	$fp = fopen("16.VEMPIREA", "rb"); fseek($fp, 0x23);
+	//$fp = fopen("16.VEMPIREA", "rb"); fseek($fp, 0x23);
 	//$fp = fopen("16.THEEND", "rb"); fseek($fp, 0x23);
+
+// 0000000: 10 00 01 00 25 00 04 00 00 00 00 00 00 00 00 00
+// 10 00 01 00 signature
+// 25 00       37 frames in this video (each frame has header of 11 bytes)
+
+// 0000010: 02 00 [9c 23] 00 00 10 00 02 00 02 [80 02] [e0 01] [00 00] [e0 01]
+// 9c 23       0x239c bytes in this frame
+// 80 02       640 pixels wide
+// e0 01       480 pixels high
+// 00 00       starting at line 0
+// e0 01       ending at line 480
+
+// 00026c0: 01 00 01 00 00 00 10 00 03 00 03 <- an example of an empty frame
 
 	$f = fread($fp, 500000);
 
@@ -45,3 +58,4 @@
 
 	print "offset  $offset\n";
 	imagepng($im, "a.png");
+
